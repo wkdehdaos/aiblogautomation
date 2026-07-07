bd5d7c74637ad24b43b654039da72c45341bd3f6
wkdehdaos <jjangda895@gmail.com>
2026-07-08 07:05:22 +0900

    auto: 2026-07-08 07:05:21

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
