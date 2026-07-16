2abc3291c207ae97b9cf6e9fc86020cf28d8427e
wkdehdaos <jjangda895@gmail.com>
2026-07-17 06:43:45 +0900

    auto: 2026-07-17 06:43:45

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import { getSession } from '@/lib/auth'
 import { prisma } from '@/lib/db'
 import { redirect } from 'next/navigation'
+import BetaConfigPanel from '@/components/BetaConfigPanel'
 
 export default async function AdminPage() {
   const session = await getSession()
